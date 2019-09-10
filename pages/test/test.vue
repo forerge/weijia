83c2165e8e0417095b4fd73fb1df5db26afea2ba
weige <916+948336@qq.com>
2019-09-10 23:29:25 +0800

    w

---
+++ pages/test/test.vue
@@ -0,0 +1,23 @@
+<template>
+	<view>
+		<dzyGridTemplat/>
+	</view>
+</template>
+ 
+<script>
+	import dzyGridTemplat from '../../components/dzy-grid-templat/dzy-grid-templat.vue'
+	export default { 
+		components:{
+			dzyGridTemplat
+		},
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
