3d293333a9fbb6adf403abb366d4cef5487baed4
weige <916+948336@qq.com>
2019-09-29 20:39:18 +0800

    a

--- common/config.js
+++ common/config.js
@@ -1,5 +1,5 @@
 const serverUrl="http://192.168.2.212:80/www_weijia/";
-const serverImgUrl="http://192.168.2.212:80/www_weijia/public/weixin-img/";
+const serverImgUrl="http://www.itinhs.com/public/weixin-img/";
 
 export default{ 
 	serverUrl,serverImgUrl
